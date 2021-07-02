bd71d431bbd0cdf29248e27673b4837686e16e79
DalekUnUniUm <62376070+DalekUnUniUm@users.noreply.github.com>
2021-07-02 17:07:03 +0200

    Add files via upload

--- RewardCentral/rewardcentral-mvc/src/main/java/com/rewardcentral/rewardcentralmvc/controller/RewardPointController.java
+++ RewardCentral/rewardcentral-mvc/src/main/java/com/rewardcentral/rewardcentralmvc/controller/RewardPointController.java
@@ -1,7 +1,19 @@
 package com.rewardcentral.rewardcentralmvc.controller;
 
+import com.rewardcentral.rewardcentralmvc.service.RewardPointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RewardPointController {
+
+    @Autowired
+    private RewardPointService rewardPointService ;
+
+    @GetMapping("/getAttractionRewardPoints")
+    public int getAttractionRewardPoints(){
+        return rewardPointService.getAttractionRewardPoints() ;
+    }
+
 }
--- RewardCentral/rewardcentral-mvc/src/main/java/com/rewardcentral/rewardcentralmvc/service/RewardPointService.java
+++ RewardCentral/rewardcentral-mvc/src/main/java/com/rewardcentral/rewardcentralmvc/service/RewardPointService.java
@@ -2,6 +2,15 @@ package com.rewardcentral.rewardcentralmvc.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class RewardPointService {
+
+    public int getAttractionRewardPoints(){
+
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 1000);
+        return randomInt;
+    }
+
 }
